dc72dfdea1c31daab619dcb43e5cfc2d78843030
David Castner <davidjcastner@gmail.com>
2017-04-10 13:57:04 -0400

    Initialize Arduino File
    Added an outline for our project code

---
+++ main.ino
@@ -0,0 +1,40 @@
+/*
+ * Get the input form the button board
+ * The input will be a int that represents 4 nibbles
+ * 
+ * @return user input
+ */
+uint16_t getUserInput() {
+  return 0;
+}
+
+/*
+ * Encodes the message using a Caesar cipher
+ * 
+ * @param message - the decoded message
+ * @return encoded message
+ */
+uint16_t encodeMessage(uint16_t message) {
+  return 0;
+}
+
+/*
+ * Decode the message based on the caesar cipher
+ * 
+ * @param message - the encoded message
+ * @return decoded message
+ */
+uint16_t decodeMessage(uint16_t message) {
+  return 0;
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+}
