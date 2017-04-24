8be86f83f6eede1f34e4080eec22c5b99657b8df
Lily Romano <lilyheart@users.noreply.github.com>
2017-04-24 17:12:56 -0400

    update mainWrite.ino

--- mainWrite.ino
+++ mainWrite.ino
@@ -1,10 +1,34 @@
 #include <Wire.h>
 #include "Adafruit_Trellis.h"
 
+#define STATE_UID 0
+#define STATE_MSG 1
+#define STATE_ACK 2
+
 Adafruit_Trellis matrix0 = Adafruit_Trellis();
 Adafruit_TrellisSet trellis =  Adafruit_TrellisSet(&matrix0);
+
+uint16_t matrixValue;
+
 uint8_t writeArduinoState; //TODO uint8_t?
-boolean isSubmitButtonReleased;
+
+boolean isSubmitButtonPressed;
+uint8_t buttonState = 0;         // variable for reading the pushbutton status
+uint8_t lastButtonState;
+const uint8_t ledPin = 99;
+const uint8_t LED_UID = 3;
+const uint8_t LED_MSG = 4;
+const uint8_t LED_ERR = 13;
+const uint8_t bigButtonPin = 2;     // the number of the pushbutton pin
+
+uint16_t validUserIDs[3][2] = {
+  {4680, 3},
+  {38505, 6},
+  {42405, 8}
+};
+
+uint8_t userShift;
+
 
 /*
  *  Code to control Trellis LEDs
@@ -17,12 +41,11 @@ void trellisAllLEDsOff() {
   trellis.writeDisplay();
 }
 
-//TODO Is it on release or press?
-void trellisButtonRelease() {
+void trellisButtonPress() {
   if (trellis.readSwitches()) { // if a button was released
+    digitalWrite(LED_ERR, LOW);
     for (uint8_t i=0; i<16; i++) { // determine which button
       if (trellis.justPressed(i)) {
-        //TODO Test LED toggles
         if (trellis.isLED(i)){
           trellis.clrLED(i);
         } else {
@@ -38,7 +61,73 @@ void trellisButtonRelease() {
  * Read trellis LEDs
  */
 
- //TODO
+void getMatrixInput() {
+  for (uint8_t i=0; i<16; i++) {
+    uint8_t position = 15 - i;
+    if (trellis.isLED(i)) {
+      matrixValue = (1 << position) | matrixValue;
+    } else {
+      matrixValue = (~(1 << position)) & matrixValue;
+    }
+  }
+}
+
+/*
+ *  Big Button Press and Big Button LED control
+ */
+void bigButtonPress() {
+  buttonState = digitalRead(bigButtonPin);
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+    if(lastButtonState != buttonState) {
+      isSubmitButtonPressed = true;
+    }
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+  }
+  lastButtonState = buttonState;
+}
+
+/*
+ *  Authentication methods
+ */
+boolean authenticateUserID() {
+  for (uint8_t i=0; i<(sizeof validUserIDs / sizeof validUserIDs[0]); i++) {
+    if(matrixValue == validUserIDs[i][0]){
+      userShift = validUserIDs[i][1];
+      return true;
+    }
+  }
+  return false;
+}
+
+/*
+ *  Authentication methods
+ */
+void updateReadArduinoStatus(uint8_t newStatus) {
+  writeArduinoState = newStatus;
+  switch (writeArduinoState) {
+    case STATE_UID: //Waiting for user ID
+      Serial.println("STATE_UID");
+      digitalWrite(LED_UID, HIGH);
+      digitalWrite(LED_MSG, LOW);
+      break;
+    case STATE_MSG:
+      Serial.println("STATE_MSG");
+      digitalWrite(LED_UID, HIGH);
+      digitalWrite(LED_MSG, LOW);
+      break;
+    case STATE_ACK:
+      Serial.println("STATE_ACK");
+      digitalWrite(LED_UID, LOW);
+      digitalWrite(LED_MSG, LOW);
+      break;
+    default:
+      break;
+  }
+}
 
 void setup() {
   // initilze serial connection
@@ -48,57 +137,63 @@ void setup() {
   // initlize trellis board
   trellis.begin(0x70);
 
-  // TODO Test LED sequential LEDs
   // light up trellis board sequentially to indicate initalization
-  int trellisLEDBoard[] = {12, 8, 4, 0, 13, 9, 5, 1, 14, 10, 6, 2, 15, 11, 7, 3};
   for (uint8_t i=0; i<16; i++) {
-    trellis.setLED(trellisLEDBoard);
+    trellis.setLED(i);
     trellis.writeDisplay();
     delay(50);
   }
 
-  //TODO Test trellisAllLEDsOff
+  delay(150);
   trellisAllLEDsOff();
+
+  //Initalize BigButton
+  pinMode(bigButtonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+
+
 
   //Initalize variables
   //TODO Test writeArduinoState init
-  writeArduinoState = 1;
-  isSubmitButtonReleased = false;
+  updateReadArduinoStatus(STATE_UID);
+  isSubmitButtonPressed = false;
+  digitalWrite(LED_ERR, LOW);
 }
 
 void loop() {
   // Background code
+  delay(20);
+  bigButtonPress();
 
   // State Code
   switch (writeArduinoState) {
-    case 1: //Waiting for user ID
-      //TODO Test trellisButtonRelease
-      trellisButtonRelease();
-      if (isSubmitButtonReleased){
-        // getMatrixInput();
-        // authenticateUserID();
-        // if (userIsAuthenticated){
-          // resetMatrix();{
-          // updateReadArduinoStatus(INPUTSTATE);
-          // move to state 2
-        // } else {
+    case STATE_UID: //Waiting for user ID
+      trellisButtonPress();
+      if (isSubmitButtonPressed){
+        isSubmitButtonPressed = false; //TODO Put this somewhere better.
+        getMatrixInput();
+        if (authenticateUserID()){
+          trellisAllLEDsOff();
+          updateReadArduinoStatus(STATE_MSG);
+        } else {
+          digitalWrite(LED_ERR, HIGH);
           // updateErrorLED(INPUTSTATE);
           // resetMatrix();
           // stay in state 1
-        // }
+        }
       }
       break;
-    // case 2: //Waiting for message
-      // trellisButtonRelease();
-      // if (isSubmitButtonReleased){
-        // getMatrixInput();
+    case STATE_MSG: //Waiting for message
+      trellisButtonPress();
+        if (isSubmitButtonPressed){
+          getMatrixInput();
         // encodeMessage();
         // sendMessage();{
         // updateReadArduinoStatus(INPUTSTATE);
         // move to state 3
-      // }
-      // break;
-    // case 3: // Waiting for acknowledge
+        }
+      break;
+    case STATE_ACK: // Waiting for acknowledge
       // if(isMessageReceived) {
         // parseCRAP();
         // if(isMessageSuccessful) {
@@ -109,7 +204,7 @@ void loop() {
           // move to state 1
         // }
       // }
-      // break;
+       break;
     default:
       break;
   }
